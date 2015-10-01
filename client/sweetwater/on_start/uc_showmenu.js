48c25f4717611e4298e44b0d0316404ebc7cf5cc
Thomas Edhag <thomas.edhag@smhi.se>
2015-10-01 15:39:03 +0200

    menues and layout

--- client/sweetwater/on_start/uc_showmenu.js
+++ client/sweetwater/on_start/uc_showmenu.js
@@ -6,12 +6,31 @@ define([
              Mustache,
              ShowMenuTemplate) {
   
+  var toggle = function() {
+      console.log('toggle');
+      var ele = document.getElementById('menu-items');
+
+      if(ele.style.display == "block") {
+        ele.style.display = "none";
+      }
+      else {
+        ele.style.display = "block";
+      }
+	};
+  
   var init = function (element){
     /* Load menu css */
     CssUtil.load('sweetwater/on_start/showmenu.css');
     
     var template = Mustache.render(ShowMenuTemplate);
     $(element).append(template);
+    
+    /*Prepare more label for action */
+    $("#menu-header").on('click', function(e){
+        console.log("click");
+        toggle();
+    });
+    
   }
   
   return {
