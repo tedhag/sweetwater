1ecf43a681d03a7a5c8e829dfe505f0e3c792d21
Thomas Edhag <thomas.edhag@smhi.se>
2016-03-18 16:58:12 +0100

    new uc

---
+++ bluewater/uc_presentstate/js/main.js
@@ -0,0 +1,25 @@
+require.config({
+  paths: {
+    'header': '../../page_header/page_header',
+    'jquery': 'lib/jquery-2.2.0.min',
+    'leaflet': 'lib/leaflet-0.7.7/leaflet',
+    'esri': 'lib/esri-leaflet',
+    'mustache': 'lib/mustache.min',
+    'text': 'lib/text',
+    'esri': 'lib/esri-leaflet',
+    'proj4': 'lib/proj4js/proj4-compressed',
+    'proj4leaflet': 'lib/proj4js/proj4leaflet',
+    'shp': 'lib/shp.min',
+    'geojsonvt': 'lib/geojson-vt-dev'
+  }
+});
+
+require(['header', 'uc_presentstate'
+], function (Header, UC_PresentState) {
+   
+    Header.init( function(){
+      console.log("init page");
+      UC_PresentState.init();
+    });
+  
+});
