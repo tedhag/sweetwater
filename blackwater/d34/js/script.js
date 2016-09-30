c1cae237de467677554120f1546e55b995bb7be1
jerrySmhi <jerry.sahlin@smhi.se>
2016-09-30 14:20:20 +0200

    d3

--- blackwater/d34/js/script.js
+++ blackwater/d34/js/script.js
@@ -1,34 +1,56 @@
 
-var width = 420,
-    barHeight = 20;
+var width = 960,
+    height = 500;
 
-var x = d3.scaleLinear()
-    .range([0, width]);
+var margin = {top: 20, right: 30, bottom: 30, left: 40},
+    width = 960 - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom;
+
+var y = d3.scaleLinear().range([height, 0]),
+    x = d3.scaleBand().rangeRound([0,width],.1),
+    xAxis = d3.axisBottom().scale(x),
+    yAxis = d3.axisLeft().scale(y).ticks(10,"%");
+
+
+var chart = d3.select(".chart")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
 
 
 //SVG Chart
 d3.tsv("js/data.txt",type, function(error, data) {
-  x.domain([0, d3.max(data, function(d) { return d.value; })]);
+    x.domain(data.map(function(d) { return d.name; }));
+    y.domain([0, d3.max(data, function(d) { return d.value; })]);
+    
+  chart.append("g")
+      .attr("class", "x axis")
+      .attr("transform", "translate(0," + height + ")")
+      .call(xAxis);
 
-var chart = d3.select(".svgchart")
-    .attr("width", width);
+
+  chart.append("g")
+      .attr("class", "y axis")
+      .call(yAxis)
+    .append("text")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 6)
+        .attr("dy", ".71em")
+        .style("text-anchor", "end")
+        .text("Frequency");
+    
+  chart.selectAll(".bar")
+      .data(data)
+    .enter().append("rect")
+      .attr("class", "bar")
+      .attr("x", function(d) { return x(d.name); })
+      .attr("y", function(d) { return y(d.value); })
+      .attr("height", function(d) { return height - y(d.value); })
+      .attr("width", x.bandwidth());    
 
 
-var bar = chart.selectAll("g")
-    .data(data)
-  .enter().append("g")
-    .attr("transform", function(d, i) { return "translate(0," + i * barHeight + ")"; });
-
-bar.append("rect")
-    .attr("width", function(d) { return x(d.value); })
-    .attr("height", barHeight - 1);
-
-bar.append("text")
-    .attr("x", function(d) { return x(d.value) - 3; })
-    .attr("y", barHeight / 2)
-    .attr("dy", ".35em")
-    .text(function(d) { return d.value; });
 });
 
 function type(d) {
