20f18886e4b64f07aa6f2360b909d29e17c2944c
jerrySmhi <jerry.sahlin@smhi.se>
2016-09-30 11:01:32 +0200

    d3

---
+++ blackwater/d34/js/script.js
@@ -0,0 +1,20 @@
+var data = [4, 8, 15, 16, 23, 42];
+
+
+d3.select(".chart")
+  .selectAll("div")
+    .data(data)
+  .enter().append("div")
+    .style("width", function(d) { return d * 10 + "px"; })
+    .text(function(d) { return d; });
+
+var chart = d3.select(".chart");
+var bar = chart.selectAll("div");
+
+var barUpdate = bar.data(data);
+
+var barEnter = barUpdate.enter().append("div");
+
+barEnter.style("width", function(d) { return d * 10 + "px"; });
+
+barEnter.text(function(d) { return d; });
